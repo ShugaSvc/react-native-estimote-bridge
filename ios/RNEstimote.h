768841ac311f8fcc2c268a3f1924efaf32b8cf8b
irvingdp <irvingdp@gmail.com>
2019-03-31 00:13:09 +0800

    fix bug fail issue.

--- ios/RNEstimote.h
+++ ios/RNEstimote.h
@@ -1,13 +1,7 @@
 
-#if __has_include("RCTBridgeModule.h")
-#import "RCTBridgeModule.h"
-#import "RCTEventEmitter.h"
-#import "RCTLog.h"
-#else
 #import <React/RCTBridgeModule.h>
 #import <React/RCTEventEmitter.h>
 #import <React/RCTLog.h>
-#endif
 
 #import <EstimoteProximitySDK/EstimoteProximitySDK.h>
 
